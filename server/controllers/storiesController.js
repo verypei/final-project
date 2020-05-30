fe692106b9eae063a72e30f29f44194f0fee7464
Evandy <andi_evandy@yahoo.co.id>
2020-05-30 14:36:27 +0700

    tampilkan pesan pada GET /stories/[id] jika story tidak ditemukan

--- server/controllers/storiesController.js
+++ server/controllers/storiesController.js
@@ -46,7 +46,11 @@ class storiesController{
         let id = req.params.id
         stories.findOne({where:{id}})
         .then(data=>{
-            res.status(200).json(data)
+            if(data) {
+                res.status(200).json(data)
+            } else {
+                res.status(404).json({message: 'story not found'})
+            }
         })
         .catch(err=>{
             res.status(500).json({message: "internal server error"});
